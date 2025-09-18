82636ba38c571abb2e6ed6672643ed75b96e16e6
Valeria Cardona <valecar2104@gmail.com>
2025-09-18 17:07:43 -0500

    Se modificó el modelo User.php

--- laravelpatitas/app/Models/User.php
+++ laravelpatitas/app/Models/User.php
@@ -6,32 +6,108 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Carbon\Carbon;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
+     * USER ATTRBUTES
+     * $this->attributes['id'] - int - contains the user primary key (ID)
+     * $this->attributes['name'] - string - contains the user name
+     * $this->attribute['email] - string - contains the user email
+     * $this->attributes['phone'] - string - contains the user telephone number
+     * $this->attributes['address'] - string - contains the user address
+     * $this->attributes['password'] - string - contains the user password
+     * $this->attribute['role_id'] - int - contains the foreign key role (ID)
+     * $this->attributes['created_at'] - timestamp - contains the created date
+     * $this->attributes['updated_at'] - timestamp - contains the updated date
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+
+    protected $table = 'users';
+    protected $fillable = ['name', 'email', 'phone', 'password', 'address', 'role_id'];
+    public $timestamps = true;
 
     /**
      * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
+    */
     protected $hidden = [
         'password',
         'remember_token',
     ];
+
+    public function getId() : int
+    {
+        return $this->attributes['id'];
+    }
+
+    public function setId(int $id): void
+    {
+        $this->attributes['id'] = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->attributes['name'];
+    }
+
+    public function setName(string $name): void
+    {
+        $this->attributes['name'] = $name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->attributes['email'];
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->attributes['email'] = $email;
+    }
+
+    public function getPhone(): string
+    {
+        return $this->attributes['phone'];
+    }
+
+    public function setPhone(string $phone): void
+    {
+        $this->attributes['phone'] = $phone;
+    }
+
+    public function getAddress(): string
+    {
+        return $this->attributes['address'];
+    }
+
+    public function setAddress(string $address): void
+    {
+        $this->attributes['address'] = $address;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->attributes['password'] = Hash::make($password);
+    }
+
+    public function getCreatedAt(): Carbon
+    {
+        return $this->attributes['created_at'];
+    }
+
+public function getUpdatedAt(): Carbon
+    {
+        return $this->attributes['updated_at'];
+    }
+
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class, 'role_id');
+    }
 
     /**
      * Get the attributes that should be cast.
