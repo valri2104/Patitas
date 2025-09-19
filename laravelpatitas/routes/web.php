f5993c9e67abd88843f3fe32822495a1bd3bc41b
Valeria Cardona <valecar2104@gmail.com>
2025-09-19 16:39:47 -0500

    Se corrió el comando 'php artisan ui bootstrap --auth '

--- laravelpatitas/routes/web.php
+++ laravelpatitas/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
+Auth::routes();
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
