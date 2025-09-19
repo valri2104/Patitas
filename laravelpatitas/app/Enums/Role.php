aaa34a8457e61b116d0df4fcacf54af93a981544
Valeria Cardona <valecar2104@gmail.com>
2025-09-19 09:42:03 -0500

    Se actualizaron los valores de Role.php a minusculas para mayor consistencia

--- laravelpatitas/app/Enums/Role.php
+++ laravelpatitas/app/Enums/Role.php
@@ -4,7 +4,7 @@ namespace App\Enums;
 
 enum Role: string
 {
-    case ADMIN = 'Admin';
-    case BUYER = 'Buyer';
-    case VETERINARIAN = 'Veterinarian';
+    case Admin = 'admin';
+    case Buyer = 'buyer';
+    case Veterinarian = 'veterinarian';
 }
