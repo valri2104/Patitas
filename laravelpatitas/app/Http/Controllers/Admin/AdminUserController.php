8de7bba1fb4c73e80a60d99843c9a16c3574ba3a
Valeria Cardona <valecar2104@gmail.com>
2025-09-23 13:21:33 -0500

    Eliminación de comentarios obsoletos en AdminUserController para mejorar la claridad del código y mantener la base de código más limpia.

--- laravelpatitas/app/Http/Controllers/Admin/AdminUserController.php
+++ laravelpatitas/app/Http/Controllers/Admin/AdminUserController.php
@@ -9,9 +9,6 @@ use App\Http\Controllers\Controller;
 
 class AdminUserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index(): View
     {
         $viewData = [];
@@ -22,49 +19,31 @@ class AdminUserController extends Controller
         return view('admin.user.index')->with('viewData', $viewData);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
