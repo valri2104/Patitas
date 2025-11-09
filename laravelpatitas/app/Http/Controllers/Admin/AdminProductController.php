d527ad1ead2e1fb2b70df6bbb719cc3ef1cf57ba
Valeria Cardona <valecar2104@gmail.com>
2025-11-09 16:02:03 -0500

    Refactoriza el controlador AdminProductController para utilizar AdminProductRequest en lugar de Request, simplificando la validación de datos al usar el método validated().

--- laravelpatitas/app/Http/Controllers/Admin/AdminProductController.php
+++ laravelpatitas/app/Http/Controllers/Admin/AdminProductController.php
@@ -12,6 +12,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use app\Enums\Category;
+use App\Http\Requests\AdminProductRequest;
 
 class AdminProductController extends Controller
 {
@@ -36,17 +37,9 @@ class AdminProductController extends Controller
         return view('admin.product.create')->with('viewData', $viewData);
     }
 
-    public function store(Request $request): RedirectResponse
+    public function store(AdminProductRequest $request): RedirectResponse
     {
-        $validatedData = $request->validate([
-            'name'         => 'required|string|max:255|unique:products,name',
-            'description'  => 'required|string',
-            'price'        => 'required|numeric|min:0',
-            'stock'        => 'required|integer|min:0',
-            'category'     => 'required|in:Alimento,Juguetes,Medicina,Accesorios',
-            'customizable' => 'boolean',
-            'imageUrl'     => 'nullable|string',
-        ]);
+        $validatedData = $request->validated();
 
         $validatedData['customizable'] = $validatedData['customizable'] ?? false;
 
@@ -92,19 +85,11 @@ class AdminProductController extends Controller
         return view('admin.product.edit')->with('viewData', $viewData);
     }
 
-    public function update(Request $request, int $id): RedirectResponse
+    public function update(AdminProductRequest $request, int $id): RedirectResponse
     {
         $product = Product::findOrFail($id);
 
-        $validatedData = $request->validate([
-            'name'         => 'required|string|max:255|unique:products,name,' . $id,
-            'description'  => 'required|string',
-            'price'        => 'required|numeric|min:0',
-            'stock'        => 'required|integer|min:0',
-            'category'     => 'required|in:Alimento,Juguetes,Medicina,Accesorios',
-            'customizable' => 'boolean',
-            'imageUrl'     => 'nullable|string',
-        ]);
+        $validatedData = $request->validated();
 
         $validatedData['customizable'] = $validatedData['customizable'] ?? false;
 
