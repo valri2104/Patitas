095470172e9fbc395db9555e018dab1fc09af492
Valeria Cardona <valecar2104@gmail.com>
2025-11-09 14:27:05 -0500

    Se agregó la relación de producto en el modelo de Review para mejorar la gestión de datos relacionados entre reseñas y productos.

--- laravelpatitas/app/Models/Review.php
+++ laravelpatitas/app/Models/Review.php
@@ -64,4 +64,9 @@ class Review extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
