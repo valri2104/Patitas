a5e41580c87c2e00acce3a2c735e9e6af228ec58
Camilo <Carbelaezr@eafit.edu.co>
2025-11-10 22:26:55 -0500

    archivo de localización para partner

---
+++ laravelpatitas/resources/lang/en/partner.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'title'         => 'Partner Products',
+    'subtitle'      => 'Supplements from our partner team',
+    'info'          => 'These products are provided by our supplements partner team',
+    'view_product'  => 'View on partner site',
+    'no_products'   => 'Partner products are not available right now',
+    'error_loading' => 'Error loading partner products',
+    'try_again'     => 'Try again',
+    'back_catalog'  => 'Back to catalog',
+    'partner_badge' => 'Partner Product',
+];
+
