89fad498e7d097b2c9fbec17cfd6f8670c01e5c2
Camilo <Carbelaezr@eafit.edu.co>
2025-11-10 19:38:19 -0500

    Correccion de errores en las vistas y estilos

--- laravelpatitas/resources/views/emails/appointment_confirmation.blade.php
+++ laravelpatitas/resources/views/emails/appointment_confirmation.blade.php
@@ -5,37 +5,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ __('appointments.confirmation_email_title') }}</title>
-
+    <link href="{{ asset('/css/email.css') }}" rel="stylesheet">
 </head>
 
-<body>
-    <div class="header">
+<body class="appointment-email-body">
+    <div class="appointment-email-header">
         <h1>{{ __('appointments.confirmation_email_title') }}</h1>
     </div>
 
-    <div class="content">
+    <div class="appointment-email-content">
         <p>{{ __('appointments.confirmation_greeting') }},</p>
 
         <p>{{ __('appointments.confirmation_message') }}</p>
 
-        <div class="detail-row">
-            <span class="detail-label">{{ __('appointments.pet_name') }}:</span> {{ $appointment->getPetName() }}
+        <div class="appointment-email-detail-row">
+            <span class="appointment-email-detail-label">{{ __('appointments.pet_name') }}:</span> {{ $appointment->getPetName() }}
         </div>
 
-        <div class="detail-row">
-            <span class="detail-label">{{ __('appointments.pet_type') }}:</span> {{ $appointment->getPetType() }}
+        <div class="appointment-email-detail-row">
+            <span class="appointment-email-detail-label">{{ __('appointments.pet_type') }}:</span> {{ $appointment->getPetType() }}
         </div>
 
-        <div class="detail-row">
-            <span class="detail-label">{{ __('appointments.date') }}:</span> {{ $appointment->getDate() }}
+        <div class="appointment-email-detail-row">
+            <span class="appointment-email-detail-label">{{ __('appointments.date') }}:</span> {{ $appointment->getDate() }}
         </div>
 
-        <div class="detail-row">
-            <span class="detail-label">{{ __('appointments.time') }}:</span> {{ $appointment->getTime() }}
+        <div class="appointment-email-detail-row">
+            <span class="appointment-email-detail-label">{{ __('appointments.time') }}:</span> {{ $appointment->getTime() }}
         </div>
 
-        <div class="detail-row">
-            <span class="detail-label">{{ __('appointments.reason') }}:</span> {{ $appointment->getReason() }}
+        <div class="appointment-email-detail-row">
+            <span class="appointment-email-detail-label">{{ __('appointments.reason') }}:</span> {{ $appointment->getReason() }}
         </div>
 
         <p>{{ __('appointments.confirmation_footer') }}</p>
@@ -43,7 +43,7 @@
         <p>{{ __('appointments.confirmation_regards') }},<br>{{ __('appointments.team_name') }}</p>
     </div>
 
-    <div class="footer">
+    <div class="appointment-email-footer">
         <p>{{ __('appointments.confirmation_footer_note') }}</p>
     </div>
 </body>
